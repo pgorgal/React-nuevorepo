49b374db9807a300c5846b3e6cf4ba595f902d63
pgorgal <pgorgal@gmail.com>
2023-12-14 14:06:09 -0300

    segunda preentrega

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -6,8 +6,8 @@ function Inicio() {
     return (
         <div>
         <div id="box">
-            <p>Desde chico siempre disfruté de hacer cosas con mis manos y lo que comenzó siendo un hobby, se volvió un pequeño emprendimiento.</p>
-            <p>Ya hace unos años que me dedico a trabajar con madera haciendo todo tipo de trabajos como muebles en madera o estilo industrial, restauraciones y también tablas.</p>
+            <p>Desde chico siempre disfruté de hacer cosas con mis manos y lo que comenzó siendo un hobby, se convirtió en un pequeño emprendimiento.</p>
+            <p>Ya hace unos años que me dedico a trabajar con madera haciendo todo tipo de trabajos, como muebles en madera o estilo industrial, restauraciones y también tablas.</p>
         </div>
         <Galeria />
         </div>
