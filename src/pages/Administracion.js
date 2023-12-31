19474a57e35646253eede40bd3518f993956e732
pgorgal <pgorgal@gmail.com>
2023-12-31 12:33:33 -0300

    Entrega Final

--- src/pages/Administracion.js
+++ src/pages/Administracion.js
@@ -2,6 +2,7 @@ import { db } from "../index"
 import { collection, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import Card from 'react-bootstrap/Card'
+import "../styles/Administracion.css"
 
 function Administracion() {
 
@@ -34,7 +35,7 @@ function Administracion() {
     }
 
     const borrarProducto = async (id) => {
-        const itemDoc=doc(db, "productos", id)
+        const itemDoc = doc(db, "productos", id)
         await deleteDoc(itemDoc)
         getProductos()
     }
@@ -42,14 +43,14 @@ function Administracion() {
     return (
         <div>
             <div>
-                <p>Nuevo producto</p>
-                <input placeholder="Descripción" onChange={(e) => setNewDescripcion(e.target.value)} />
-                <input placeholder="Imagen" onChange={(e) => setNewImg(e.target.value)} />
-                <input placeholder="Material" onChange={(e) => setNewMaterial(e.target.value)} />
-                <input placeholder="Medidas" onChange={(e) => setNewMedidas(e.target.value)} />
-                <input placeholder="Nombre" onChange={(e) => setNewNombre(e.target.value)} />
-                <input placeholder="Precio" type="number" onChange={(e) => setNewPrecio(e.target.value)} />
-                <button onClick={onSubmitProducto} >Enviar</button>
+                <h3>Nuevo producto</h3>
+                <input className="input" placeholder="Descripción" onChange={(e) => setNewDescripcion(e.target.value)} />
+                <input className="input" placeholder="Imagen" onChange={(e) => setNewImg(e.target.value)} />
+                <input className="input" placeholder="Material" onChange={(e) => setNewMaterial(e.target.value)} />
+                <input className="input" placeholder="Medidas" onChange={(e) => setNewMedidas(e.target.value)} />
+                <input className="input" placeholder="Nombre" onChange={(e) => setNewNombre(e.target.value)} />
+                <input className="input" placeholder="Precio" type="number" onChange={(e) => setNewPrecio(e.target.value)} />
+                <button className="agregar" onClick={onSubmitProducto} >Enviar</button>
             </div>
             <div className="tarjetola">
                 {productos.map((producto) => {
@@ -57,11 +58,13 @@ function Administracion() {
                         <Card key={producto.id} className="column tarjetas" id="card" style={{ width: '12rem' }}>
                             <Card.Img variant="top" src={producto.img} alt={producto.descripcion} className="img" />
                             <Card.Body>
-                                <Card.Title className="card-title">Nombre: {producto.nombre}</Card.Title>
+                                <Card.Title className="card-title">{producto.nombre}</Card.Title>
                                 <Card.Text className="card-prize">Precio: ${producto.precio} </Card.Text>
                                 <Card.Text className="card-prize">Medidas: {producto.medidas} </Card.Text>
                                 <Card.Text className="card-prize">Material: {producto.material} </Card.Text>
-                                <button onClick={() => borrarProducto(producto.id)}>Borrar</button>
+                                <div className="btn">
+                                    <button className="borrar" onClick={() => borrarProducto(producto.id)}>Borrar</button>
+                                </div>
                             </Card.Body>
                         </Card>
                     )
