d2ece7a3ef90050cb929f86d9e83efc760455d52
pgorgal <pgorgal@gmail.com>
2024-01-12 20:43:15 -0300

    Entrega Final

--- src/components/CarritoItem.js
+++ src/components/CarritoItem.js
@@ -1,19 +1,24 @@
 import { useContext } from "react"
 import "../styles/CarritoItem.css"
 import { CarritoContext } from "../Context/CarritoContext"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const CarritoItem = ({ nombre, precio, id, q, img, descripcion }) => {
 
   const { quitar } = useContext(CarritoContext)
 
   return (
-    <div className="boxCarrito">
-      <img src={img} alt={descripcion} className="fotito" />
-      <p>{nombre}</p>
-      <p>Cantidad: {q}</p>
-      <p>Subtotal: {precio * q}</p>
-      <button onClick={() => quitar(id)} id="detalle">Eliminar</button>
-    </div>
+    <Container id="boxCarrito">
+      <Row>
+        <Col><img src={img} alt={descripcion} className="fotito" /></Col>
+        <Col><p>{nombre}</p></Col>
+        <Col><p>Cantidad: {q}</p></Col>
+        <Col><p>Subtotal: {precio * q}</p></Col>
+        <Col><button onClick={() => quitar(id)} id="detalle">Eliminar</button></Col>
+      </Row>
+    </Container>
   )
 }
 
