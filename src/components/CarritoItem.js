f2943dbf6f96c43eefcc3242b40f1445beb19f68
pgorgal <pgorgal@gmail.com>
2024-01-13 09:01:36 -0300

    Entrega Final

--- src/components/CarritoItem.js
+++ src/components/CarritoItem.js
@@ -7,12 +7,12 @@ const CarritoItem = ({ nombre, precio, id, q, img, descripcion }) => {
   const { quitar } = useContext(CarritoContext)
 
   return (
-    <div className="boxCarrito">
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(5, 1fr)" }} className="boxCarrito">
       <img src={img} alt={descripcion} className="fotito" />
       <p>{nombre}</p>
       <p>Cantidad: {q}</p>
       <p>Subtotal: {precio * q}</p>
-      <button onClick={() => quitar(id)} id="detalle">Eliminar</button>
+      <button onClick={() => quitar(id)} id="detalleCarrito">X</button>
     </div>
   )
 }
